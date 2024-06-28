3d45eca2c8ffe12a1bb68a5afcd39cff4252fdf6
pranay2406 <pranaysam8@gmail.com>
2024-06-28 13:59:18 +0530

    Advanced Singleton Concept(Thread)

--- src/DesignPattern/Singleton/Main.java
+++ src/DesignPattern/Singleton/Main.java
@@ -11,5 +11,13 @@ public class Main {
 
         Singleton s3 = Singleton.getObject();
         System.out.println(s3);
+
+
+         SingletonAdv sa1 = SingletonAdv.getObjectAdv();
+        System.out.println(sa1);
+
+        SingletonAdv sa2 = SingletonAdv.getObjectAdv();
+        System.out.println(sa2);
+
     }
 }
--- src/DesignPattern/Singleton/SingletonAdv.java
+++ src/DesignPattern/Singleton/SingletonAdv.java
@@ -1,4 +1,28 @@
 package DesignPattern.Singleton;
 
+import lombok.Synchronized;
+
 public class SingletonAdv {
+    private static SingletonAdv instanceAdv;
+
+    private SingletonAdv(){
+
+    }
+       public static SingletonAdv getObjectAdv()
+       {
+            if(instanceAdv == null)
+            {
+                synchronized (SingletonAdv.class)
+                {
+                    if(instanceAdv==null)
+                    {
+                        instanceAdv = new SingletonAdv();
+                    }
+
+                }
+
+            }
+        return instanceAdv;
+       }
+
 }
